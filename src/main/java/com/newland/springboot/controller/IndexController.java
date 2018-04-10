a3320ed156014651a11848bcf626dca487edf296
qyw <m18759158365@163.com>
2018-04-10 11:53:50 +0800

    add beetl and static files

--- src/main/java/com/newland/springboot/controller/IndexController.java
+++ src/main/java/com/newland/springboot/controller/IndexController.java
@@ -4,24 +4,29 @@ import com.github.pagehelper.PageInfo;
 import com.newland.springboot.domain.User;
 import com.newland.springboot.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController  
+@Controller
 public class IndexController {
 
-  @Autowired
-  private IUserService userService;
-  
-    @RequestMapping(value = "/",method = {RequestMethod.HEAD,RequestMethod.GET})
-    public String home() {  
-        return "Hello World!";  
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value = "/", method = {RequestMethod.HEAD, RequestMethod.GET})
+    public String home() {
+        return "index";
     }
 
     @GetMapping("/users")
-    public PageInfo<User> getUsers(@RequestParam(value = "pageNum",defaultValue="1") int pageNum,@RequestParam(value = "pageNum",defaultValue="1") int pageSize) {
+    @ResponseBody
+    public PageInfo<User> getUsers(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum, @RequestParam(value = "pageNum", defaultValue = "1") int pageSize) {
         //使用分页插件,核心代码就这一
 
-        return userService.getUsers(pageNum,pageSize);
+        return userService.getUsers(pageNum, pageSize);
     }
+
+
+
 
 }  
